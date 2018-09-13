f58c28a860db554d1738c78276c47312e245446b
fortheday <fortheday@126.com>
2018-09-13 11:01:26 +0800

    上传、生成excel 下载

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -7,9 +7,105 @@ use models\Order;
 
 class UserController
 {
-    public function test()
+    public function uploadbig()
     {
-        sleep(100);
+        /* 接收提交的数据 */
+        $count = $_POST['count'];  // 总的数量
+        $i = $_POST['i'];        // 当前是第几块
+        $size = $_POST['size'];   // 每块大小
+        $name = 'big_img_'.$_POST['img_name'];  // 所有分块的名字
+        $img = $_FILES['img'];    // 图片
+        /* 保存每个分片 */
+        move_uploaded_file( $img['tmp_name'] , ROOT.'tmp/'.$i);
+        /* 最后一个图片上传成功之后，合并所有图片 */
+        // 思考：如何判断是否所有图片都已经上传成功？
+        // 难点：因为每个分块到达服务器的顺序不固定，所以我们不能根据顺序来判断是否都上传成功。
+        // 实现思路：每上传一个就+1，直到上传的数量等于总的数量
+        $redis = \libs\Redis::getInstance();
+        // 每上传一张就加1
+        $uploadedCount = $redis->incr($name);
+        // 如果是最后一个分支就合并
+        if($uploadedCount == $count)
+        {
+            // 以追回的方式创建并打开最终的大文件
+            $fp = fopen(ROOT.'public/uploads/big/'.$name.'.png', 'a');
+            // 循环所有的分片
+            for($i=0; $i<$count; $i++)
+            {
+                // 读取第 i 号文件并写到大文件中
+                fwrite($fp, file_get_contents(ROOT.'tmp/'.$i));
+                // 删除第 i 号临时文件
+                unlink(ROOT.'tmp/'.$i);
+            }
+            // 关闭文件
+            fclose($fp);
+            // 从 redis 中删除这个文件对应的编号这个变量
+            $redis->del($name);
+        }
+    }
+
+    public function uploadall()
+    {
+        /* 先创建目录 */
+        $root = ROOT.'public/uploads/';
+        // 今天日期
+        $date = date('Ymd');  // 20180913
+        // 如果没有这个目录就创建目录
+        if(!is_dir($root . $date))
+        {
+            // 创建目录（第二个参数：有写的权限（只对 Linux 系统)）
+            mkdir($root . $date, 0777);
+        }
+
+        // 循环五张图片的 name
+        foreach($_FILES['images']['name'] as $k => $v)
+        {
+            $name = md5( time() . rand(1,9999) );
+            $ext = strrchr($v, '.');    // .jpg
+            $name = $name . $ext;
+            // 根据 name 的下标找到对应的临时文件并移动
+            move_uploaded_file($_FILES['images']['tmp_name'][$k], $root . $date .'/' . $name);
+            echo $root . $date .'/' . $name . '<hr>';
+        }
+    }
+
+    public function album()
+    {
+        view('users.album');
+    }
+    public function setavatar()
+    {
+        /* 先创建目录 */
+        $root = ROOT.'public/uploads/';
+        // 今天日期
+        $date = date('Ymd');  // 20180913
+        // 如果没有这个目录就创建目录
+        if(!is_dir($root . $date))
+        {
+            // 创建目录（第二个参数：有写的权限（只对 Linux 系统)）
+            mkdir($root . $date, 0777);
+        }
+
+        // 生成唯一的名字
+        $name = md5( time() . rand(1,9999) );  // 32 位字符串
+
+        // 先取出原来这个图片的后缀
+        // strrchr：从最后某一个字符开始截取到最后
+        $ext = strrchr($_FILES['avatar']['name'], '.');    // .jpg
+
+        // 补上扩展名
+        $name = $name . $ext;
+
+        // 移动图片
+        move_uploaded_file($_FILES['avatar']['tmp_name'], $root . $date .'/' . $name);
+
+        echo $root . $date .'/' . $name;
+
+    }
+
+    public function avatar()
+    {
+        view('users.avatar');
     }
 
     public function orderStatus()
